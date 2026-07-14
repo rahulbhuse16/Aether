9780b235d5983cd98d40168aaed32ca2d1f42ac9
Rahul-Elma-Dev <rahul@elma.ltd>
2026-07-14 15:46:12 +0530

    changelog : build created

--- aether-backend/dist/router/github.js
+++ aether-backend/dist/router/github.js
@@ -6,5 +6,11 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const github_1 = require("../controller/github");
 const express_1 = __importDefault(require("express"));
 const gitHubRouter = express_1.default.Router();
-gitHubRouter.get('/connect', github_1.connectGithubAccount);
+gitHubRouter.post('/connect', github_1.connectGithubAccount);
+gitHubRouter.get("/test", (_, res) => {
+    res.json({
+        success: true,
+        message: "GitHub router working",
+    });
+});
 exports.default = gitHubRouter;
