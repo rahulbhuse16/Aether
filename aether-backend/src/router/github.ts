bbf6167300315a9fdeea0b6dd2de534a4e54ecc3
Rahul-Elma-Dev <rahul@elma.ltd>
2026-07-14 15:41:42 +0530

    changelog : test route added

--- aether-backend/src/router/github.ts
+++ aether-backend/src/router/github.ts
@@ -3,4 +3,12 @@ import express from 'express'
 const gitHubRouter=express.Router()
 gitHubRouter.post('/connect',connectGithubAccount)
 
+
+gitHubRouter.get("/test", (_, res) => {
+  res.json({
+    success: true,
+    message: "GitHub router working",
+  });
+});
+
 export default gitHubRouter
