d54acfc8426526fd7620a3c09498b07e535c9fda
Rahul-Elma-Dev <rahul@elma.ltd>
2026-07-14 18:04:50 +0530

    changelog : route call change

--- aether-backend/src/router/auth.ts
+++ aether-backend/src/router/auth.ts
@@ -2,6 +2,6 @@ import { firebaseLogin, getUser } from "../controller/auth"
 import express from 'express'
 const authRouter=express.Router()
 authRouter.post('/firebase',firebaseLogin)
-authRouter.post('/user',getUser)
+authRouter.get('/user',getUser)
 
 export default authRouter
