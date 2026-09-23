46cb993bf8abff505dfd9bd67fbf378e3368c201
Rahul-Elma-Dev <rahul@elma.ltd>
2026-09-23 18:32:30 +0530

    changelog : elastic search implemented

--- aether-backend/src/controller/auth.ts
+++ aether-backend/src/controller/auth.ts
@@ -827,7 +827,7 @@ export const logoutOidc = (
   });
 
   const logoutUrl =
-    `${OIDC_ISSUER}/v2/logout?${params.toString()}`;
+    `${OIDC_ISSUER}v2/logout?${params.toString()}`;
 
   res.redirect(logoutUrl);
 };
